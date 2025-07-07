01fc7c86338351e189142f134ddf43999c667383
BHARATJON <bharatsurana101@gmail.com>
2025-07-07 22:34:44 +0530

    complete front end

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
 
-  const {user}= useContext(AppContext)
+  const {user, setShowLogin}= useContext(AppContext)
 
 
   const navigate=useNavigate()
@@ -39,7 +39,7 @@ const Navbar = () => {
           :
           <div className='flex items-center ga-2 sm:gap-5'>
             <p onClick={()=>navigate('/buy')} className='cursor-pointer'> pricing</p>
-            <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>login</button>
+            <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full cursor-pointer' onClick={()=>setShowLogin(true)}>login</button>
           </div>
         }
 
