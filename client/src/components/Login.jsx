415e975d7d717a395b4e8e6a28ef385a8d9cd342
BHARATJON <bharatsurana101@gmail.com>
2025-07-09 18:45:04 +0530

    start merge front and back end.(time 5:26:00)

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,10 +3,54 @@ import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import { motion } from 'motion/react'
 
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
 const Login = () => {
 
     const [state, setState] = useState('Login')
-    const {setShowLogin}=useContext(AppContext)
+    const {setShowLogin, backendUrl, setToken, setUser}=useContext(AppContext)
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const onSubmitHandler = async (e) =>{
+        e.preventDefault();
+
+        try {
+            if(state === 'login'){
+                const {data} = await axios.post(backendUrl +'/api/user/login', {email,password})
+
+                if(data.success){
+                    setToken(data.token)
+                    setUser(data.user)
+                    localStorage.getItem('token', data.token)
+                    setShowLogin(false)
+                }else{
+                    toast.erroe(data.message)
+                }
+            }else{
+
+                const {data} = await axios.post(backendUrl +'/api/user/register', {name, email,password})
+
+                if(data.success){
+                    setToken(data.token)
+                    setUser(data.user)
+                    localStorage.getItem('token', data.token)
+                    setShowLogin(false)
+                }else{
+                    toast.erroe(data.message)
+                }
+
+            }
+        } catch (error) {
+            toast.erroe(data.message)
+        }
+    }
+
+
 
     useEffect(()=>{
         document.body.style.overflow='hidden';
@@ -20,7 +64,7 @@ const Login = () => {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
 
-        <motion.form
+        <motion.form  onSubmit={onSubmitHandler}
         
         initial={{opacity:0.2, y:50}}
         transition={{duration:0.3}}
@@ -34,17 +78,17 @@ const Login = () => {
 
             {state != 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
                 <img src={assets.profile_icon} alt="" className='max-h-4'/>
-                <input type="text" className='outline-none text-sm' placeholder='Full Name' required/>
+                <input onChange={e => setName(e.target.value)} value={name} type="text" className='outline-none text-sm' placeholder='Full Name' required/>
             </div>}
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.email_icon} alt="" />
-                <input type="email" className='outline-none text-sm' placeholder='Email id' required/>
+                <input onChange={e => setEmail(e.target.value)} value={email} type="email" className='outline-none text-sm' placeholder='Email id' required/>
             </div>
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.lock_icon} alt="" />
-                <input type="password" className='outline-none text-sm' placeholder='Password' required/>
+                <input onChange={e => setPassword(e.target.value)} value={password} type="password" className='outline-none text-sm' placeholder='Password' required/>
             </div>
             <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password</p>
             <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state==='Login'?'Login':'create account'}</button>
